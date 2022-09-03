002dcb007a37be893a9fc08448ac3915ff56da88
Samuel Belete <samyethio891@gmail.com>
2022-09-02 23:52:06 -0700

    docker file added

--- app.js
+++ app.js
@@ -8,31 +8,46 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + "/public"));
 
-const connection = mysql.createConnection({
-	host: "localhost",
-	user: "root",
-	password: password,
-	database: "join_us",
-});
+//COPY package.json .
 
-app.get("/", function(req, res){
+// const connection = mysql.createConnection({
+// 	host: "localhost",
+// 	user: "root",
+// 	password: password,
+// 	database: "join_us",
+// });
+let users= [
+	{ id: 1, username: "samule@me", email: "sam@gmail.com" },
+	{ id: 1, username: "samule@me", email: "sam@gmail.com" },
+	,
+	{ id: 1, username: "su@me", email: "sam@gmail.com" },
+	,
+	{ id: 1, username: "me_me", email: "sam@gmail.com" },
+	,
+	{ id: 1, username: "sam_sam", email: "sam@gmail.com" },
+];
+app.get("/", function (req, res) {
     // Find count of users in DB
-    const q = "SELECT COUNT(*) AS count FROM users";
-    connection.query(q, function(err, results){
-        if(err) throw err;
-        const count = results[0].count; 
-        res.render("home", {count: count});
-    });
+    // const q = "SELECT COUNT(*) AS count FROM users";
+    // connection.query(q, function(err, results){
+    //     if(err) throw err;
+    //     const count = results[0].count;
+    //     res.render("home", {count: count});
+    // });
+    res.render("home", { count: 200 });
 });
 app.get('/all-users',(req,res)=>{
-    let q = 'select * from users';
-    connection.query(q,(err,results)=>{
-        if(err) throw err;
-        res.render('users',{
-            users:results
-        })
-        return;
-    })
+    // let q = 'select * from users';
+    // connection.query(q,(err,results)=>{
+    //     if(err) throw err;
+    //     res.render('users',{
+    //         users:results
+    //     })
+    //     return;
+    // })
+    res.render("users", {
+		users:users
+	});
 })
 app.post("/register", function(req, res){
     const person = {
@@ -44,14 +59,18 @@ app.post("/register", function(req, res){
         console.log("required fields are empty");
         return res.redirect('/')
     }else{
-        console.log(person)
-        connection.query("INSERT INTO users SET ?", person, function (err, result) {
-            if (err) {
-                return res.redirect('/');
+        // console.log(person)
+        // connection.query("INSERT INTO users SET ?", person, function (err, result) {
+        //     if (err) {
+        //         return res.redirect('/');
 
-            };
-            res.redirect("/");
-        });
+        //     };
+        //     users.push(person);
+        //     res.redirect("/");
+        // });
+        console.log(person)
+        users.push(person);
+		res.redirect("/");
     }
     
 });
